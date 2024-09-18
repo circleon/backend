5ac3c7ed1fb01274a239203b32e9595877f05640
myeongha <audgkrnt@naver.com>
2024-09-18 15:36:49 +0900

    git init

---
+++ src/main/java/com/circleon/CircleOnApplication.java
@@ -0,0 +1,13 @@
+package com.circleon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/circleon/CircleOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.circleon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

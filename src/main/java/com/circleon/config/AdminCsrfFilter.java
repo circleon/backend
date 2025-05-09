5366a617a78b34fb429722c0dd24a187ed2ac571
myeongha <audgkrnt@naver.com>
2025-05-09 16:53:04 +0900

    fix: csrf fliter

--- src/main/java/com/circleon/config/AdminCsrfFilter.java
+++ src/main/java/com/circleon/config/AdminCsrfFilter.java
@@ -26,6 +26,8 @@ public class AdminCsrfFilter extends OncePerRequestFilter {
             String origin = request.getHeader("Origin");
             String referer = request.getHeader("Referer");
 
+            log.info("Origin: {}, Referer: {}", origin, referer);
+
             if( !(isValidOrigin(origin) || isValidReferer(referer)) ){
                 response.sendError(HttpServletResponse.SC_FORBIDDEN, "CSRF suspected");
                 log.warn("CSRF suspected - origin: {}, referer: {}", origin, referer);
